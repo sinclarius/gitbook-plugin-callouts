85e422f5c33c6fc1f53ace2711ed2c1fb7d412bb
Andreas Schempp <andreas.schempp@terminal42.ch>
2016-08-04 21:36:51 +0200

    Fixed support for GitBook 3

--- index.js
+++ index.js
@@ -94,140 +94,132 @@ var cheerio = require( "cheerio" )
         // It should return the new page
         page  : function ( page )
         {
-            var section
-            ,$bq
+            var $bq
             ,$this
             ,$strong
             ,style
             ;
 
-            for ( var i in page.sections ) {
-                section = page.sections[i];
-                if ( section.type !== "normal" ) {
-                    continue;
+            $ = cheerio.load( page.content );
+
+            // converting Markdown blockquotes:
+            $bq = $( "blockquote" ).each(function () {
+                $this = $(this);
+                $testheader = $this.find('h4:first-child');
+                if( !$testheader || $testheader.length == 0) {
+                    return;
                 }
 
-                $ = cheerio.load( section.content );
+                var children = $this.children().toArray();
+                $header = children.shift();
 
-                // converting Markdown blockquotes:
-                $bq = $( "blockquote" ).each(function () {
-                    $this = $(this);
-                    $testheader = $this.find('h4:first-child');
-                    if( !$testheader || $testheader.length == 0) {
-                        return;
-                    }
+                $title = $($header).text();
+                $parts = $title.split('::', 2);
 
-                    var children = $this.children().toArray();
-                    $header = children.shift();
+                var style = options[$parts[0].toLowerCase()] ?  // look up annotation in options
+                options[$parts[0].toLowerCase()] :
+                options['default'];
 
-                    $title = $($header).text();
-                    $parts = $title.split('::', 2);
+                if (!style) {
+                    return;
+                }
 
-                    var style = options[$parts[0].toLowerCase()] ?  // look up annotation in options
-                    options[$parts[0].toLowerCase()] :
-                    options['default'];
+                var icon = $('<i>')
+                .addClass('fa ' + style.picto);
 
-                    if (!style) {
-                        return;
-                    }
+                // This checks the showTypeInHeader option and if false will not show the section
+                // type when header text is present.
+                if (options["showTypeInHeader"] === true) {
+                    var title = ($parts[1] === "") ? $parts[0] : $parts.join(": ");
+                } else {
+                    var title = ($parts[1] === "") ? $parts[0] : $parts[1];
+                }
 
-                    var icon = $('<i>')
-                    .addClass('fa ' + style.picto);
-
-                    // This checks the showTypeInHeader option and if false will not show the section
-                    // type when header text is present.
-                    if (options["showTypeInHeader"] === true) {
-                        var title = ($parts[1] === "") ? $parts[0] : $parts.join(": ");
-                    } else {
-                        var title = ($parts[1] === "") ? $parts[0] : $parts[1];
-                    }
-
-                    var panelTitle = $('<h3>')
-                        .addClass('panel-title')
-                        .append(icon)
-                        .append(" " + title)
-                    ;
-                    var panelHeading = $('<div>')
-                        .addClass('panel-heading')
-                        .append(panelTitle)
-                    ;
-                    var panelBody = $('<div>')
-                        .addClass('panel-body')
-                        .append(children)
-                    ;
+                var panelTitle = $('<h3>')
+                    .addClass('panel-title')
+                    .append(icon)
+                    .append(" " + title)
+                ;
+                var panelHeading = $('<div>')
+                    .addClass('panel-heading')
+                    .append(panelTitle)
+                ;
+                var panelBody = $('<div>')
+                    .addClass('panel-body')
+                    .append(children)
+                ;
 
 
-                    var panel = $('<div>')
-                        .addClass('panel panel-' + style.alert)
-                        .append(panelHeading)
-                        .append(panelBody);
+                var panel = $('<div>')
+                    .addClass('panel panel-' + style.alert)
+                    .append(panelHeading)
+                    .append(panelBody);
 
-                    $this.before(panel);
-                    $this.remove();
+                $this.before(panel);
+                $this.remove();
 
-                    // Replace by the transformed element
-                    section.content = $.html();
-                });
+                // Replace by the transformed element
+                page.content = $.html();
+            });
 
-                // converting AsciiDoc blockquotes:
-                $bq = $( "div.quoteblock" ).each(function () {
-                    $this = $(this);
-                    children = $this.find('p');
-                    $testheader = children.first().text();
-                    children = children.toArray();
-                    children.shift();
-                    if (
-                        !$testheader ||
-                        $testheader.length === 0 ||
-                        $testheader.substr(0,5) !== '==== '
-                    ) {
-                        return;
-                    }
+            // converting AsciiDoc blockquotes:
+            $bq = $( "div.quoteblock" ).each(function () {
+                $this = $(this);
+                children = $this.find('p');
+                $testheader = children.first().text();
+                children = children.toArray();
+                children.shift();
+                if (
+                    !$testheader ||
+                    $testheader.length === 0 ||
+                    $testheader.substr(0,5) !== '==== '
+                ) {
+                    return;
+                }
 
-                    $parts = $testheader.substr(5).split('::');
+                $parts = $testheader.substr(5).split('::');
 
-                    var style = options[$parts[0].toLowerCase()] ?  // look up annotation in options
-                    options[$parts[0].toLowerCase()] :
-                    options['default'];
+                var style = options[$parts[0].toLowerCase()] ?  // look up annotation in options
+                options[$parts[0].toLowerCase()] :
+                options['default'];
 
-                    if (!style) {
-                        return;
-                    }
+                if (!style) {
+                    return;
+                }
 
-                    var icon = $('<i>').addClass('fa ' + style.picto);
+                var icon = $('<i>').addClass('fa ' + style.picto);
 
-                    if (options["showTypeInHeader"] === true) {
-                        var title = ($parts[1] === "") ? $parts[0] : $parts.join(": ");
-                    } else {
-                        var title = ($parts[1] === "") ? $parts[0] : $parts[1];
-                    }
+                if (options["showTypeInHeader"] === true) {
+                    var title = ($parts[1] === "") ? $parts[0] : $parts.join(": ");
+                } else {
+                    var title = ($parts[1] === "") ? $parts[0] : $parts[1];
+                }
 
-                    var panelTitle = $('<h3>')
-                        .addClass('panel-title')
-                        .append(icon)
-                        .append(" " + title)
-                    ;
-                    var panelHeading = $('<div>')
-                        .addClass('panel-heading')
-                        .append(panelTitle)
-                    ;
-                    var panelBody = $('<div>')
-                        .addClass('panel-body')
-                        .append(children)
-                    ;
+                var panelTitle = $('<h3>')
+                    .addClass('panel-title')
+                    .append(icon)
+                    .append(" " + title)
+                ;
+                var panelHeading = $('<div>')
+                    .addClass('panel-heading')
+                    .append(panelTitle)
+                ;
+                var panelBody = $('<div>')
+                    .addClass('panel-body')
+                    .append(children)
+                ;
 
-                    var panel = $('<div>')
-                        .addClass('panel panel-' + style.alert)
-                        .append(panelHeading)
-                        .append(panelBody);
+                var panel = $('<div>')
+                    .addClass('panel panel-' + style.alert)
+                    .append(panelHeading)
+                    .append(panelBody);
 
-                    $this.before(panel);
-                    $this.remove();
+                $this.before(panel);
+                $this.remove();
 
-                    // Replace by the transformed element
-                    section.content = $.html();
-                });
-            }
+                // Replace by the transformed element
+                page.content = $.html();
+            });
 
             return page;
         }
